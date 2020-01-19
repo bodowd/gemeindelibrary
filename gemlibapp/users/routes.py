8ef83ed3c8dc4567252e4ab59e5e21e7dcd10de5
Bing <odowd.bing@gmail.com>
2020-01-19 20:48:29 +0100

    removed no longer needed routes

--- gemlibapp/users/routes.py
+++ gemlibapp/users/routes.py
@@ -11,23 +11,6 @@ from gemlibapp.users.utils import send_reset_email
 
 users = Blueprint('users', __name__)
 
-
-# @users.route('/register', methods=['GET', 'POST'])
-# def register():
-#     if current_user.is_authenticated:
-#         return redirect(url_for('main.home'))
-#     form = RegistrationForm()
-#     if form.validate_on_submit():
-#         # hash password
-#         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-#         # create new instance of user - validation is done in forms.py
-#         user = User(username=form.username.data, email=form.email.data, password=hashed_password)
-#         db.session.add(user)
-#         db.session.commit()
-#         flash('Your account has been created! You are now able to log in', 'success')
-#         return redirect(url_for('users.login'))  # home is the name of the FUNCTION of that route
-#     return render_template('register.html', title='Register', form=form)
-
 @login_manager.request_loader
 def request_loader(request):
     email = request.form.get('email')
@@ -71,57 +54,3 @@ def logout():
     return redirect(url_for('main.home'))
 
 
-# @users.route('/account', methods=['GET', 'POST'])
-# @login_required
-# def account():
-#     ''' login_required decorator checks to see if user is logged in already'''
-#     form = UpdateAccountForm()
-#     if form.validate_on_submit():
-#         current_user.username = form.username.data
-#         current_user.email = form.email.data
-#         db.session.commit()
-#         flash('Your account has been updated!', 'success')
-#         # post-get-redirect pattern "Are you sure you want to reload? Data will be resubmitted"
-#         return redirect(url_for('users.account'))
-#     # populate form fields with current user data if method is just GET
-#     elif request.method == 'GET':
-#         form.username.data = current_user.username
-#         form.email.data = current_user.email
-#     return render_template('account.html', title='Account', form=form)
-
-
-# @users.route('/reset_password', methods=['GET', 'POST'])
-# def reset_request():
-#     # want to make sure the user is logged out before resetting password
-#     if current_user.is_authenticated:
-#         return redirect(url_for('main.home'))
-#     form = RequestResetForm()
-#     # if user has submitted a valid email, get the user
-#     if form.validate_on_submit():
-#         user = User.query.filter_by(email=form.email.data).first()
-#         send_reset_email(user)
-#         flash('An email has been sent with instructions to reset your password.', 'info')
-#         return redirect(url_for('users.login'))
-#     return render_template('reset_request.html', title='Reset Password', form=form)
-#
-#
-# @users.route('/reset_password/<token>', methods=['GET', 'POST'])
-# def reset_token(token):
-#     # want to make sure the user is logged out before resetting password
-#     if current_user.is_authenticated:
-#         return redirect(url_for('main.home'))
-#
-#     user = User.verify_reset_token(token)
-#     if user is None:
-#         flash('That is an invalid or expired token.', 'warning')
-#         return redirect(url_for('users.reset_request'))
-#     form = ResetPasswordForm()
-#     if form.validate_on_submit():
-#         # hash password
-#         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-#         # create new instance of user - validation is done in forms.py
-#         user.password = hashed_password
-#         db.session.commit()
-#         flash('Your password has been updated! You are now able to log in.', 'success')
-#         return redirect(url_for('users.login'))  # home is the name of the FUNCTION of that route
-#     return render_template('reset_token.html', title='Reset Password', form=form)
