045baebeadb2ba4094cb401ea2cf290febfcb940
Bing <odowd.bing@gmail.com>
2020-02-01 09:09:19 +0100

    added database migration

--- gemlibapp/__init__.py
+++ gemlibapp/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
 from gemlibapp.config import Config
 
 db = SQLAlchemy()
+migrate = Migrate()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
 # redirects to the login page if user is not logged in. Use with the @login_required decorator
@@ -13,11 +15,13 @@ login_manager.login_view = 'users.login'  # login is the `function` name of the 
 login_manager.login_message_category = 'info'  # info is bootstrap class for a nice blue alert
 mail = Mail()
 
+
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
 
     db.init_app(app)
+    migrate.init_app(app, db)
     bcrypt.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
@@ -35,4 +39,3 @@ def create_app(config_class=Config):
     app.register_blueprint(reminder)
 
     return app
-
