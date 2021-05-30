0747542f91570bba0d80d6def7b722c9c85c08e7
Bing <odowd.bing@gmail.com>
2021-05-30 09:08:11 +0200

    change to config.json

--- gemlibapp/config.py
+++ gemlibapp/config.py
@@ -20,3 +20,20 @@ class Config:
     APP_PASSWORD = config.get('APP_PASSWORD')
     PYTHONPATH = config.get('PYTHONPATH')
 
+# class Config:
+#     # protects against modifying cookies and cross-site forgery attacks
+#     # import secrets; secrets.token_hex(16) to generate the token
+#     SECRET_KEY = os.environ['SECRET_KEY']
+#     # with sqlite we can specify a relative path with /*3
+#     # SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'  # will be created in our project directory
+#     SQLALCHEMY_DATABASE_URI = os.environ['SQLALCHEMY_DATABASE_URI']  # will be created in our project directory
+#     MAIL_SERVER = 'smtp.googlemail.com'
+#     MAIL_PORT = 587
+#     MAIL_USE_TLS = True
+#     MAIL_USERNAME = os.environ['EMAIL_USER']
+#     MAIL_PASSWORD = os.environ['EMAIL_PASS']
+#     APP_USERNAME = os.environ['APP_USERNAME']
+#     APP_PASSWORD = os.environ['APP_PASSWORD']
+#     PYTHONPATH = os.environ['PYTHONPATH']
+#
+#
